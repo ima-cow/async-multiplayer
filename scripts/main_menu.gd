bc0ace1ea165b403c153ddb266ae7f001f0d6472
ima-cow <ethanthequag@gmail.com>
2025-12-16 15:07:05 -0500

    adding removing saves

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -45,10 +45,7 @@ func _on_host_game_pressed() -> void: #this function can error, not returned so 
 		if settings["save_names"].size() != 0:
 			for save_name in settings["save_names"]:
 				if save_name is String:
-					var save := Button.new()
-					save.text = save_name
-					save.name = save_name
-					%SaveList.add_child(save)
+					_create_save_button(save_name)
 				else:
 					printerr("Save name data is corrupted: ", Error.ERR_INVALID_DATA)
 					get_tree().paused = true
@@ -63,6 +60,7 @@ func _on_create_game_pressed() -> void:
 	if %SaveList.get_child_count() == 1 or %SaveList.get_child(1) is not LineEdit:
 		var name_field := LineEdit.new()
 		name_field.placeholder_text = "Save name..."
+		name_field.max_length = 30
 		
 		%SaveList.add_child(name_field)
 		%SaveList.move_child(name_field, 1)
@@ -72,12 +70,26 @@ func _on_create_game_pressed() -> void:
 		#afer waiting for name to be submited add it to dict and create a button 
 		settings["save_names"].push_front(name_field.text)
 		
-		var save := Button.new()
-		save.text = name_field.text
-		save.name = name_field.text
-		%SaveList.add_child(save)
-		%SaveList.move_child(save, 1)
+		_create_save_button(name_field.text)
 		name_field.queue_free()
+
+
+func _create_save_button(save_name: String) -> void:
+	#create a button for a save and delete and connect signals
+	var save_container := HBoxContainer.new()
+	
+	var save_button := Button.new()
+	save_button.text = save_name
+	save_button.name = save_name
+	save_button.custom_minimum_size.x = 307
+	
+	var delete_save_button := Button.new()
+	delete_save_button.text = "X"
+	
+	%SaveList.add_child(save_container)
+	%SaveList.move_child(save_container, 1)
+	save_container.add_child(save_button)
+	save_container.add_child(delete_save_button)
 
 
 func _on_join_game_pressed() -> void:
@@ -108,5 +120,11 @@ func _save_settings() -> Error:
 	
 	if !settings_file.store_var(settings):
 		return Error.ERR_FILE_CANT_WRITE
+	
+	return Error.OK
+
+
+func _on_save_button_pressed() -> Error:
+	var err: Error
 	
 	return Error.OK
