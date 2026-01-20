90d6458d01c9265a04fe158d1a62ffd3b3b00d65
ima-cow <ethanthequag@gmail.com>
2026-01-20 10:58:22 -0500

    no idea

---
+++ scripts/proc_gen.gd
@@ -0,0 +1,16 @@
+extends Node
+
+const tile_texture:Texture2D = preload("res://assets/tile.png")
+var texture_sixe := tile_texture.get_size()
+var tile_sprite := Sprite2D.new()
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	GraphNode
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
